7cedf8580f0b1d21d024090d33e279b577f10d71
jenkins_onetec <jenkins@grupoonetec.com>
2023-05-12 12:31:34 +0200

    init repository

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,9 @@
+public class Launcher {
+  public static void main(String[] args) {
+    System.out.println(" _____ _     _      ____  _  ____  _      ____ ");
+    System.out.println("/    // \\ /\\/ \\  /|/   _\\/ \\/  _ \\/ \\  /|/  _ \\");
+    System.out.println("|  __\\| | ||| |\\ |||  /  | || / \\|| |\\ ||| / \\|");
+    System.out.println("| |   | \\_/|| | \\|||  \\__| || \\_/|| | \\||| |-||");
+    System.out.println("\\_/   \\____/\\_/  \\|\\____/\\_/\\____/\\_/  \\|\\_/ \\|");
+  }
+}
